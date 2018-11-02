336e32927bbdb85498b38c83854dae5a1eeb2f45
venkateshmantha <manthavenkatesh@gmail.com>
2018-11-01 20:29:58 -0400

    changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,14 +6,18 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $session = $request->session();
+
         return view('card')->with([
-            'units' => ['Meter', 'Kilometer', 'Mile', 'Centimeter', 'Millimeter', 'Inch', 'Foot', 'Yard'],
-            'roundUp' => 'off',
-            'hasErrors' => false,
-            'errors' => [],
-            'alert' => 0
+            'units' => ['Meter', 'Kilometer', 'Mile', 'Centimeter', 'Millimeter', 'Inch', 'Foot'],
+            'from' => $session->get('from'),
+            'to' => $session->get('to'),
+            'val' => $session->get('val'),
+            'roundUp' => $session->get('roundUp'),
+            'alert' => $session->get('alert'),
+            'res' => $session->get('res')
         ]);
     }
 
@@ -21,17 +25,100 @@ class HomeController extends Controller
     {
         $from = $request->get('from');
         $to = $request->get('to');
-        $value = $request->get('value');
+        $val = $request->get('value');
         $roundUp = $request->get('roundUp');
 
         $request->validate([
             'value' => 'required|numeric'
         ]);
 
+        $res = $this->getResult($from, $to, $val);
+
+        if ($roundUp == 'on') {
+            $res = (int)$res;
+        }
+
         return redirect('/')->with([
             'from' => $from,
             'to' => $to,
-            'value' => $value
+            'val' => $val,
+            'res' => $res,
+            'alert' => 1,
+            'roundUp' => $roundUp
         ]);
+    }
+
+    public function getResult(String $fromUnit, String $toUnit, float $value)
+    {
+        $baseUnitVal = $this->convertToMeter($fromUnit, $value);
+        $convertedVal = $this->getLengthConvert($toUnit, $baseUnitVal);
+
+        return $convertedVal;
+    }
+
+    public function convertToMeter(String $fromUnit, float $value)
+    {
+        $baseval = 0.0;
+        switch ($fromUnit) {
+            case 'Meter':
+                $baseval = $value;
+                break;
+            case 'Kilometer':
+                $baseval = $value * 1000;
+                break;
+            case 'Mile':
+                $baseval = $value * 1609.34;
+                break;
+            case 'Centimeter':
+                $baseval = $value * 0.01;
+                break;
+            case 'Millimeter':
+                $baseval = $value * 0.001;
+                break;
+            case 'Inch':
+                $baseval = $value * 0.0254;
+                break;
+            case 'Foot':
+                $baseval = $value * 0.3048;
+                break;
+            case 'Yard':
+                $baseval = $value * 0.9144;
+                break;
+        }
+
+        return $baseval;
+    }
+
+    public function getLengthConvert(String $toUnit, float $value)
+    {
+        $conval = 0.0;
+        switch ($toUnit) {
+            case 'Meter':
+                $conval = $value;
+                break;
+            case 'Kilometer':
+                $conval = $value * 0.001;
+                break;
+            case 'Mile':
+                $conval = $value * 0.000621371;
+                break;
+            case 'Centimeter':
+                $conval = $value * 100;
+                break;
+            case 'Millimeter':
+                $conval = $value * 1000;
+                break;
+            case 'Inch':
+                $conval = $value * 39.3701;
+                break;
+            case 'Foot':
+                $conval = $value * 3.28084;
+                break;
+            case 'Yard':
+                $conval = $value * 1.09361;
+                break;
+        }
+
+        return $conval;
     }
 }
--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -19,43 +19,48 @@
                                 <div class='col'>
                                     <label>From</label>
                                     <select name='from' class='form-control'>
-                                        <?php foreach ($units as $unit): ?>
-                                        <option <?php if (isset($from) && $from == $unit) echo 'selected' ?>><?= $unit ?></option>
-                                        <?php endforeach; ?>
+                                        @foreach ($units as $unit)
+                                            <option @if (isset($from) && $from == $unit) {{'selected'}} @endif >{{$unit}}</option>
+                                        @endforeach
                                     </select>
                                 </div>
                                 <div class='col'>
                                     <label>To</label>
                                     <select name='to' class='form-control'>
-                                        <?php foreach ($units as $unit): ?>
-                                        <option <?php if (isset($to) && $to == $unit) echo 'selected' ?>><?= $unit ?></option>
-                                        <?php endforeach; ?>
+                                        @foreach ($units as $unit)
+                                            <option @if (isset($to) && $to == $unit) {{'selected'}} @endif >{{$unit}}</option>
+                                        @endforeach
                                     </select>
                                 </div>
                             </div>
-                            <input class='form-control mt-4'
+                            <label class='mt-4' for="value">Value to convert</label>
+                            <input class='form-control mt-2'
                                    type='text'
                                    name='value'
-                                   value='<?php echo (isset($val)) ? $val : '' ?>'
+                                   id='value'
+                                   value='{{ $val ?? ''}}'
                                    placeholder='Value to convert'>
-                            <label><input class='mt-4' type='checkbox'
-                                          name='roundUp' <?php if ($roundUp == 'on') echo 'checked' ?> > Round up</label><br>
+                            <label class='mt-4' for="roundup">RoundUp</label>
+                            <input type='checkbox'
+                                   name='roundUp'
+                                   id='roundup'
+                            @if ($roundUp == 'on') {{'checked'}} @endif ><br>
                             <button type='submit' class='btn btn-light mt-4'>Convert</button>
                             <button name='clear' class='btn btn-dark mt-4'>Clear</button>
                         </form>
-                        <?php if ($hasErrors): ?>
-                        <div class='errors alert alert-danger mt-4'>
-                            <?php foreach ($errors as $error): ?>
-                                <?= $error ?>
-                            <?php endforeach; ?>
-                        </div>
-                        <?php endif ?>
-                        <?php if (!$hasErrors && $alert == 1): ?>
-                        <div class='alert alert-success mt-4' role='alert'>
-                            <?php echo 'Result: ' . $val . ' ' . $from . ' = ' .
-                                $result . ' ' . $to ?>
-                        </div>
-                        <?php endif; ?>
+                        @if(count($errors) > 0)
+                            <div class='errors alert alert-danger mt-4'>
+                                @foreach ($errors->all() as $error)
+                                    {{ $error }}
+                                @endforeach
+                            </div>
+                        @endif
+                        @if($alert == 1)
+                            <div class='alert alert-success mt-4' role='alert'>
+                                {{'Result: ' . $val . ' ' . $from . ' = ' .
+                                    $res . ' ' . $to }}
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
